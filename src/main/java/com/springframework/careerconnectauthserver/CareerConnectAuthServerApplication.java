c9248e10c4e0654fe9887c5932048b5d09dfd06e
Kaan Çolakoğlu <kaancolakoglu3@gmail.com>
2024-11-29 23:56:34 +0300

    initial commit: Spring Authorization Server setup

---
+++ src/main/java/com/springframework/careerconnectauthserver/CareerConnectAuthServerApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.careerconnectauthserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CareerConnectAuthServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CareerConnectAuthServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/springframework/careerconnectauthserver/CareerConnectAuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframework.careerconnectauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CareerConnectAuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
